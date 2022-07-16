0ec455dbaa4692a75aed2c3cb7f9aef70b4e80bb
Hokori23 <hokori23@qq.com>
2022-07-16 18:14:53 +0800

    chore: init project

---
+++ src/components/DemoComponent.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { ref } from 'vue';
+import { NButton } from 'naive-ui';
+
+const count = ref(0);
+</script>
+<template>
+  <NButton type="primary" @click="() => count++"> count++: {{ count }} </NButton>
+</template>
