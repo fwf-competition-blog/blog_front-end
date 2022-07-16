61fdb683a8fdb975d6c6be8c332cf98ffc5d7019
Hokori23 <hokori23@qq.com>
2022-07-16 18:49:42 +0800

    chore: 添加vue-router & pinia & 框架代码

---
+++ src/router/index.js
@@ -0,0 +1,10 @@
+import VueRouter from 'vue-router';
+const routes = [
+  // { path: '/', component: Home },
+  // { path: '/about', component: About },
+];
+
+export default VueRouter.createRouter({
+  history: VueRouter.createWebHistory(),
+  routes,
+});
