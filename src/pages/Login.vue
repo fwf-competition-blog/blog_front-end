cc74e2bd5ee8979d5a4f3d2b40042263215d98b4
陈舒莹 <10703457+zzangyou@user.noreply.gitee.com>
2022-08-13 23:31:40 +0800

    git

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,2 +1,291 @@
-<script setup></script>
-<template>登陆页</template>
+<!-- <script setup>
+import { RouterView } from 'vue-router';
+/* import { defineComponent, ref } from 'vue';
+import {
+  useMessage
+} from 'naive-ui'; */
+
+</script> -->
+<script>
+import { defineComponent, ref } from 'vue';
+import { useMessage } from 'naive-ui';
+import { PersonCircle } from '@vicons/ionicons5'
+export default defineComponent({
+   components: {
+    // eslint-disable-next-line vue/no-unused-components
+  PersonCircle
+  },
+  setup() {
+   const formRef = ref(null);
+    const rPasswordFormItemRef = ref(null);
+    const message = useMessage();
+    const modelRef = ref({
+      age: null,
+      password: null,
+      reenteredPassword: null
+    });
+    function validatePasswordStartWith(rule, value) {
+      return !!modelRef.value.password && modelRef.value.password.startsWith(value) && modelRef.value.password.length >= value.length;
+    }
+    function validatePasswordSame(rule, value) {
+      return value === modelRef.value.password;
+    }
+    const rules = {
+      age: [
+        {
+          required: true,
+          validator(rule, value) {
+            if (!value) {
+              return new Error('需要年龄');
+            } else if (!/^\d*$/.test(value)) {
+              return new Error('年龄应该为整数');
+            } else if (Number(value) < 18) {
+              return new Error('年龄应该超过十八岁');
+            }
+            return true;
+          },
+          trigger: ['input', 'blur']
+        }
+      ],
+      password: [
+        {
+          required: true,
+          message: '请输入密码'
+        }
+      ],
+    };
+    return {
+       PersonCircle,
+      formRef,
+      rPasswordFormItemRef,
+      model: modelRef,
+      rules,
+      handlePasswordInput() {
+        if (modelRef.value.reenteredPassword) {
+          rPasswordFormItemRef.value?.validate({ trigger: 'password-input' });
+        }
+      },
+      handleValidateButtonClick(e) {
+        e.preventDefault();
+        formRef.value?.validate((errors) => {
+          if (!errors) {
+            message.success('验证成功');
+          } else {
+            console.log(errors);
+            message.error('验证失败');
+          }
+        });
+      }
+    };
+  }
+});
+</script>
+<template>
+
+<div id="root">
+    <div class="App" style="min-height: 754px;">
+        <div class="Sign flex flex-column">
+          <div class="non-select jss1 css-1vbrzl8">
+            <h3 class="css-1eezi6a">
+              Blog
+            </h3>
+          </div>
+          <div class="ka-wrapper">
+              <div class="ka-content">
+                  <div class="jss2 css-ykq3zm">
+                    <n-form ref="formRef" :model="model" :rules="rules" class="flex flex-column">
+                     <h4 class="non-select jss3 css-1pyxybg">登录</h4>
+    <n-form-item path="age" label="年龄">
+      <span class="logo-box">
+          <n-icon size="30" :depth="3" class="logo">
+    <PersonCircle />
+  </n-icon>
+      </span>
+      <n-input v-model:value="model.age" @keydown.enter.prevent />
+
+    </n-form-item>
+    <n-form-item path="password" label="密码">
+      <n-input
+        v-model:value="model.password"
+        type="password"
+        @input="handlePasswordInput"
+        @keydown.enter.prevent
+      />
+    </n-form-item>
+    <n-row :gutter="[0, 24]">
+      <n-col :span="24">
+        <div style="display: flex; justify-content: flex-end">
+          <n-button
+            :disabled="model.age === null"
+            round
+            type="primary"
+            @click="handleValidateButtonClick"
+          >
+            验证
+          </n-button>
+        </div>
+      </n-col>
+    </n-row>
+  </n-form>
+
+  <pre>{{ JSON.stringify(model, null, 2) }}
+</pre>
+
+
+
+
+                  </div>
+              </div>
+          </div>
+        </div>
+        </div>
+</div>
+</template>
+<style scoped>
+.flex-column {
+    flex-direction: column;
+}
+.flex {
+    display: flex;
+}
+#root, .App, body, html {
+    height: 100%;
+    width: 100%;
+}
+.App {
+    display: flex;
+    flex-direction: column;
+}
+.Sign {
+    height: 100%;
+    width: 100%;
+    padding-top: 8rem;
+    align-items: center;
+    justify-content: center;
+}
+.jss1 {
+    padding: 1rem 2.5rem;
+}
+
+.non-select {
+    -webkit-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+}
+.css-1vbrzl8 {
+    background-color: rgb(255, 255, 255);
+    color: rgb(0, 0, 0);
+    transition: box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+    border-radius: 4px;
+    box-shadow: none;
+}
+.css-1eezi6a {
+    margin: 0px;
+    font-family: Roboto, Helvetica, Arial, sans-serif;
+    font-weight: 400;
+    font-size: 3rem;
+    line-height: 1.167;
+    letter-spacing: 0em;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    color: rgb(63, 81, 181);
+}
+.ka-content, .ka-wrapper {
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+}
+.ka-wrapper {
+    height: 100%;
+}
+.css-ykq3zm {
+    background-color: rgb(255, 255, 255);
+    color: rgb(0, 0, 0);
+    transition: box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+    border-radius: 4px;
+    box-shadow: rgb(0 0 0 / 20%) 0px 2px 1px -1px, rgb(0 0 0 / 14%) 0px 1px 1px 0px, rgb(0 0 0 / 12%) 0px 1px 3px 0px;
+}
+
+.css-1pyxybg {
+    margin: 0px;
+    font-family: Roboto, Helvetica, Arial, sans-serif;
+    font-weight: 400;
+    font-size: 2.125rem;
+    line-height: 1.235;
+    letter-spacing: 0.00735em;
+    color: rgb(63, 81, 181);
+}
+.jss2 {
+    padding: 0.5rem 3rem 2rem 3rem;
+    background-color: #fafafa;
+}
+.jss3 {
+    margin: 2rem 0;
+    text-align: center;
+}
+.jss4 {
+    margin-bottom: 1.5rem;
+}
+.css-i44wyl {
+    display: inline-flex;
+    flex-direction: column;
+    position: relative;
+    min-width: 0px;
+    padding: 0px;
+    margin: 0px;
+    border: 0px;
+    vertical-align: top;
+}
+.css-uadgdw.Mui-error {
+    color: rgb(211, 47, 47);
+}
+.css-uadgdw {
+    color: rgb(117, 117, 117);
+    font-family: Roboto, Helvetica, Arial, sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+    line-height: 1.4375em;
+    letter-spacing: 0.00938em;
+    padding: 0px;
+    display: block;
+    transform-origin: left top;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    max-width: 133%;
+    position: absolute;
+    left: 0px;
+    top: 0px;
+    transform: translate(0px, -1.5px) scale(0.75);
+    transition: color 200ms cubic-bezier(0, 0, 0.2, 1) 0ms, transform 200ms cubic-bezier(0, 0, 0.2, 1) 0ms, max-width 200ms cubic-bezier(0, 0, 0.2, 1) 0ms;
+
+}
+.css-sp68t1.Mui-error {
+    color: rgb(211, 47, 47);
+}
+.css-135qv08 {
+    font-family: Roboto, Helvetica, Arial, sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+    line-height: 1.4375em;
+    letter-spacing: 0.00938em;
+    color: rgb(0, 0, 0);
+    box-sizing: border-box;
+    cursor: text;
+    display: inline-flex;
+    -webkit-box-align: center;
+    align-items: center;
+    position: relative;
+}
+label + .css-135qv08 {
+    margin-top: 16px;
+}
+.logo-box{
+  padding: 2px 0 0 0;
+}
+.logo{
+  margin: 0 4px;
+}
+
+</style>
